c878f2211a7f787a6326ea5af30cc0ae02ab43a7
Pedro <pedroricarthgc@gmail.com>
2025-04-16 20:24:21 -0300

    Adição de novos componentes e incluindo formulario de login

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, TextInput, View } from 'react-native';
 import Logo from './componentes/Logo';
-import UserLogin from './componentes/UserLogin';
+import CustomButton from './componentes/CustomButton';
+
+const image = require("./assets/Background1.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Logo/>
-      <UserLogin/>
-      <StatusBar style="auto" />
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <View style={styles.overlay}>
+          <Logo />
+          <View style={styles.form}>
+            <Text style={styles.loginText}>Faça Login Abaixo</Text>
+            <Text style={styles.text}>Usuário</Text>
+            <TextInput style={styles.input} placeholder="Digite seu usuário"/>
+            <Text style={styles.text}>Senha</Text>
+            <TextInput style={styles.input} placeholder="Digite sua senha"/>
+          </View>
+          <CustomButton title="Login" onPress={() => alert("Botão pressionado")} />
+        </View>
+        <StatusBar style="auto" />
+      </ImageBackground>
     </View>
   );
 }
@@ -17,7 +30,45 @@ const styles = StyleSheet.create({
   container:{
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: ''
-  }
-})
+    justifyContent: 'center'
+  },
+
+  image:{
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center'
+  },
+
+  overlay: {
+    flex: 1,  
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    width: '100%', 
+  },
+
+  form: {
+    width: '80%',
+    marginBottom: 20
+  },
+
+  input:{
+    height: 40,
+    marginVertical: 8,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    backgroundColor: '#ffffff'
+  },
+
+  text:{
+    fontSize: 14,
+    color: '#000'
+  },
+
+  loginText:{
+    marginBottom: 20,
+    fontSize: 16,
+    textAlign: 'center',
+    fontWeight: 'bold'
+  },
+});
