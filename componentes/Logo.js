44242da4b76bfdc948fe788893d36ed9aaf72c05
Pedro <pedroricarthgc@gmail.com>
2025-04-22 20:15:31 -0300

    Alteração na logo

--- componentes/Logo.js
+++ componentes/Logo.js
@@ -1,18 +1,43 @@
-import { Image, StyleSheet, Text, View } from "react-native"
+import { Image, StyleSheet, Text, View } from "react-native";
 
-const logo = require("../assets/agenda-cem.png")
+const logo = require("../assets/GestSport_Logo.png");
 
 const Logo = () => {
-    return <View>
-        <Image style={styles.logo} source={logo} resizeMode="contain"/>
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={logo} resizeMode="contain" />
+      <Text style={styles.text}>Bem Vindo Ao{"\n"} GestSport</Text>
+      <Text style={styles.description}>
+        Acompanhe e gerencie a agenda{"\n"}da Secretaria de Esportes da sua cidade
+      </Text>
     </View>
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    logo:{
-        width: 200,
-        height: 200
-    }
-})
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 30
+  },
+  logo: {
+    width: 600,
+    marginBottom: 10, 
+  },
+  text: {
+    fontFamily: "Outfit-Bold",
+    fontSize: 28,
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  description: {
+    fontFamily: "Outfit-Regular",
+    fontSize: 18,
+    textAlign: "center",
+    paddingHorizontal: 20,
+    padding: 30,
+    color: '#969696'
+  },
+});
 
 export default Logo;
