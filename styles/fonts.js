edfca92064013b5f2c0d7825d51b327f43451002
Pedro <pedroricarthgc@gmail.com>
2025-04-22 20:15:42 -0300

    arquivo para fontes

---
+++ styles/fonts.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from 'react-native';
+
+const fonts = StyleSheet.create({
+  thin: {
+    fontFamily: 'Outfit-Thin',
+  },
+  extraLight: {
+    fontFamily: 'Outfit-ExtraLight',
+  },
+  light: {
+    fontFamily: 'Outfit-Light',
+  },
+  regular: {
+    fontFamily: 'Outfit-Regular',
+  },
+  medium: {
+    fontFamily: 'Outfit-Medium',
+  },
+  semiBold: {
+    fontFamily: 'Outfit-SemiBold',
+  },
+  bold: {
+    fontFamily: 'Outfit-Bold',
+  },
+  extraBold: {
+    fontFamily: 'Outfit-ExtraBold',
+  },
+  black: {
+    fontFamily: 'Outfit-Black',
+  },
+});
+
+export default fonts;
